ebd94d6e29dc4444a55c972e4ae10b1734299d2b
RickyS <35142826+abliskan@users.noreply.github.com>
2021-12-07 16:45:59 +0700

    Update DQLab chapter 1

--- Python for Data Professional Beginner - Part 1/module.py
+++ Python for Data Professional Beginner - Part 1/module.py
@@ -1,3 +1,35 @@
 #Chapter1 - Mari mengenal python
+"""
 ##Program pertama: "Hello World"
 print("Hello World!")
+
+##Program Pertamaku
+print("Riset Bahasa Python")
+##Struktur Program Python - Part 1
+### Statement
+print("Belajar Python menyenangkan") 
+print("Halo Dunia")
+print("Hello World!")
+### Variables & Literals
+bilangan1 = 5
+bilangan2 = 10
+kalimat1 = "Belajar Bahasa Python"
+### Operators
+print(bilangan1 + bilangan2)
+
+##Tugas Praktek - 1
+bilangan1 = 20
+bilangan2 = 10
+print(bilangan1 - bilangan2)
+"""
+
+##Tugas Praktek - 2
+harga_asli = 20000
+potongan = 2000
+harga_setelah_potongan = harga_asli - potongan
+harga_final = harga_setelah_potongan * 1.1
+print(harga_final)
+
+##Quiz 1
+
+##Quiz 2
