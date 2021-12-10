f8c93da1c1bca5b0f84cd67473d5f17e0c1ebbb4
RickyS <35142826+abliskan@users.noreply.github.com>
2021-12-10 09:25:20 +0700

    Add Chapter 3 Dqlab Study Note

--- Python for Data Professional Beginner - Part 1/module.py
+++ Python for Data Professional Beginner - Part 1/module.py
@@ -98,4 +98,22 @@ total_pajak = total_harga * 0.1
 ### Cetak total_harga + total_pajak
 print(total_harga+total_pajak)
 """
+#Chapter3 - Pythons Conditioning & Looping
+## Python conditioning for decision - part 1
 
+## Python conditioning for decision - part 2
+# Statement if
+x = 4
+if x % 2 == 0: # jika sisa bagi x dengan 2 sama dengan 0
+    print("x habis dibagi dua") # statemen aksi lebih menjorok ke dalam
+# Statement if ... elif ... else
+x = 7
+if x % 2 == 0: # jika sisa bagi x dengan 2 sama dengan 0
+    print("x habis dibagi dua")
+elif x % 3 == 0: # jika sisa bagi x dengan 3 sama dengan 0
+    print("x habis dibagi tiga")
+elif x % 5 == 0: # jika sisa bagi x dengan 5 sama dengan 0
+    print("x habis dibagi lima")
+else:
+    print("x tidak habis dibagi dua, tiga ataupun lima")
+ 
