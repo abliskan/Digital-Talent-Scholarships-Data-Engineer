d536c09ca7d785b566871722c9fb2e6f10770493
RickyS <35142826+abliskan@users.noreply.github.com>
2021-12-15 15:27:00 +0700

    Add chapter 1 Dqlab part 2

--- Python for Data Professional Beginner - Part 2/module.py
+++ Python for Data Professional Beginner - Part 2/module.py
@@ -1,1 +1,64 @@
-print('Hello World')
+#Chapter1 - Mari mengenal python
+"""
+
+## Mengakses List dan Tuple - Part 1
+bulan_pembelian = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember')
+print(bulan_pembelian[0])
+print(bulan_pembelian[5])
+print(bulan_pembelian[-1])
+print(bulan_pembelian[-2])
+
+## Mengakses List dan Tuple - Part 1
+bulan_pembelian = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember')
+pertengahan_tahun = bulan_pembelian[4:8]
+print(pertengahan_tahun)
+awal_tahun = bulan_pembelian[:5]
+print(awal_tahun)
+akhir_tahun = bulan_pembelian[8:]
+print(akhir_tahun)
+print(bulan_pembelian[-4:-1])
+
+## Penggabungan Dua atau Lebih List atau Tuple 
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es teh', 'Es jeruk', 'Es Campur']
+list_menu = list_makanan + list_minuman
+print(list_menu)
+
+## Quiz
+"""
+## List Manipulation – Part 1
+# Fitur .append()
+print(">>> Fitur .append()")
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan.append('Ketoprak')
+print(list_makanan)
+# Fitur .clear()
+print(">>> Fitur .clear()")
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan.clear()
+print(list_makanan)
+# Fitur .copy()
+print(">>> Fitur .copy()")
+list_makanan1 = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan2 = list_makanan1.copy()
+list_makanan3 = list_makanan1
+list_makanan2.append('Opor')
+list_makanan3.append('Ketoprak')
+print(list_makanan1)
+print(list_makanan2)
+# Fitur .count()
+print(">>> Fitur .count()")
+list_score = ['Budi', 'Sud', 'Budi', 'Budi', 'Budi', 'Sud', 'Sud']
+score_budi = list_score.count('Budi')
+score_sud = list_score.count('Sud')
+print(score_budi)
+print(score_sud)
+# Fitur .extend()
+print(">>> Fitur .extend()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu.extend(list_minuman)
+print(list_menu)
+
+
+
